b7003935a6c376102b78d25777723aa0aeb98735
Владимир Соколов <sokolover.heh@gmail.com>
2024-05-22 21:25:37 +0300

    button2

--- app/src/main/java/com/example/myhealthapplication/HelpActivity.java
+++ app/src/main/java/com/example/myhealthapplication/HelpActivity.java
@@ -1,5 +1,6 @@
 package com.example.myhealthapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,6 +12,8 @@ public class HelpActivity extends AppCompatActivity {
     private TextView tvExit;
     private TextView tvHelp;
     private TextView tvIdea;
+    private TextView tvLicense;
+    private TextView tvPolitics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +21,11 @@ public class HelpActivity extends AppCompatActivity {
         tvExit = findViewById(R.id.tv_exit);
         tvHelp = findViewById(R.id.tv_help);
         tvIdea = findViewById(R.id.tv_idea);
+        tvPolitics = findViewById(R.id.tv_politics);
+        tvLicense = findViewById(R.id.tv_license);
         tvExit.setOnClickListener(v -> startActivity(new Intent(HelpActivity.this, LoginActivity.class)));
+        tvLicense.setOnClickListener(v -> setContentView(R.layout.activity_license));
+        tvPolitics.setOnClickListener(v -> setContentView(R.layout.activity_politics));
         tvHelp.setOnClickListener(v -> {
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://vk.com/sokolovvladimir"));
             startActivity(intent);
